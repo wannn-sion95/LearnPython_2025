b4bf9f259196bbbc359a01bfc62cc05099c6b905
Wannn Sion <wannnsion95@gmail.com>
2025-08-04 16:45:23 +0700

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-  el: "#apk",
-  data: {
-    name: "",
-    pesan:
-      "Aku sangat mencintaimu, tapi aku terlalu takut untuk mengungkapkannya kepadamu, karena aku masih merasa tidak pantas untuk bersanding denganmu.",
-  },
-});
