220d9f8a5327dde10bc19e94f43fa6ccffb1b2ba
Wannn Sion <wannnsion95@gmail.com>
2025-08-09 17:43:48 +0700

    Create logical_thinking.py

---
+++ logical_thinking.py
@@ -0,0 +1,8 @@
+def penjumlahan(angka_Pertama, angka_Kedua):
+  hasil_penjumlahan = angka_Pertama + angka_Kedua
+  return hasil_penjumlahan
+
+
+a = int(input("Masukkan angka Pertma: "))
+b = int(input("Masukkan angka Kedua: "))
+print(f"Hasil penjumlahan: {penjumlahan(a, b)}")
